f496f286cdc19bb6727282070d7cbadffa512d09
torhen <torhen@gmail.com>
2021-03-16 00:29:18 +0100

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+function setup() {
+    createCanvas(500, 400);
+}
+function draw() {
+    background(0, 255, 255);
+    textSize(30);
+    text('hello p5js', 100, 100);
+}
---
+++ hello.ts
@@ -0,0 +1,11 @@
+import * as bla from 'p5/global' 
+
+function setup():void {
+  createCanvas(500, 400);
+}
+
+function draw():void {
+  background(0, 255, 255);
+  textSize(30)
+  text('hello p5js', 100, 100, )
+}
